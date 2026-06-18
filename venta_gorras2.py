35951dc894ec35c8d9b3805eeb7e724183faaa12
edurios232009-dev <edu.rios232009@gmail.com>
2026-06-18 13:52:46 -0700

    Update venta_gorras2.py

--- venta_gorras2.py
+++ venta_gorras2.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.markdown("""
+<style>
+.stApp {
+    background-color: #00ff00;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.title("Fondo Verde ✅")
+st.write("Si ves el fondo verde, el CSS está funcionando!")
